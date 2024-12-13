99d5e2c0b7577e69af751400ac66de2493be56d9
zeljko pejic <129476882+pejsplay@users.noreply.github.com>
2024-12-13 10:52:26 +0100

    Update tailwind.config.ts
    Signed-off-by: zeljko pejic <129476882+pejsplay@users.noreply.github.com>

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -2,9 +2,9 @@ import type { Config } from "tailwindcss";
 
 export default {
   content: [
-    "./pages/**/*.{js,ts,jsx,tsx,mdx,html}",
-    "./components/**/*.{js,ts,jsx,tsx,mdx,html}",
-    "./app/**/*.{js,ts,jsx,tsx,mdx,html}",
+    "./pages/**/*.{js,ts,jsx,tsx,mdx,html,css}",
+    "./components/**/*.{js,ts,jsx,tsx,mdx,html,css}",
+    "./app/**/*.{js,ts,jsx,tsx,mdx,html,css}",
   ],
   theme: {
     extend: {
